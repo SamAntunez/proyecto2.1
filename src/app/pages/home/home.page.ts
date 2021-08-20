eb29b39b368fe5ca649b7a7c2bd6b05d22cb8adb
SamAntunez <84483432+SamAntunez@users.noreply.github.com>
2021-08-20 17:15:01 -0400

    CONFIRM

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router,private navControl: NavController) {}
+
+  irPagina2(){
+    this.router.navigate(['/pagina2']);
+  }
+  irPagina3(){
+    this.navControl.navigateForward(['/pagina3']);
+  }
 
 }
