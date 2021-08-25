5e8cda3ab00ee65303e5f42b16b045b94752d978
SamAntunez <84483432+SamAntunez@users.noreply.github.com>
2021-08-25 12:36:28 -0400

    confirm

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
+  valor:string="hola mundo";
 
-  constructor(private router: Router,private navControl: NavController) {}
+  constructor(private router: Router,private navControl: NavController, private menu: MenuController) {}
 
   irPagina2(){
     this.router.navigate(['/pagina2']);
